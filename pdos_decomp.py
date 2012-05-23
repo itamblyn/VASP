503fe6b6ad4a27b87b062f5459ff5f0ea5a2e4e8
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2012-05-23 16:32:21 -0700

    fixing pdos_decomp so it does a loop over atoms, adding safety features, etc

--- pdos_decomp.py
+++ pdos_decomp.py
@@ -1,125 +1,164 @@
 #! /usr/bin/env python
-
+ 
 import sys
 import commands
 import numpy
+ 
+subtractFermi = False
+ncore_electrons = 2
+nvalence_electrons = 4
+ 
+def main():
+ 
+ try:
+#     input_filename = sys.argv[1]
+     input_filename = '/Users/tamblyn2/llnl/sulfur/vasp/pdos/DOSCAR'
 
-try:
-    input_filename = sys.argv[1]
-except IndexError:
-    print '\nusage: ' + sys.argv[0] + ' input_filename'
-    print '\nexiting...\n'
-    sys.exit(0)
+ except IndexError:
+     print '\nusage: ' + sys.argv[0] + ' input_filename'
+     print '\nexiting...\n'
+     sys.exit(0)
 
-core_valence_divide = -1000 # <- all valence 
+ nelectrons = ncore_electrons + nvalence_electrons
+ 
+ command_line_counter = commands.getoutput('wc -l ' + input_filename).split()
+ 
+ if len(command_line_counter) != 2:
+     print 'Error determining file size'
+ else:
+     number_of_lines = int(command_line_counter[0])
+ 
+ outputFile_dos = open('dos.dat', 'w')
+ outputFile_pdos = open('pdos.dat', 'w')
+ 
+ inputFile = open(input_filename, 'r')
+ natom = int(inputFile.readline().split()[0])
+ 
+ for i in range(4):
+     inputFile.readline()
+ 
+ inline_total = inputFile.readline()
+ 
+ emin_total = float(inline_total.split()[1])
+ emax_total = float(inline_total.split()[0])
+ enum_total = int(inline_total.split()[2])
+ efermi_total = float(inline_total.split()[3])
+ 
+ dos = numpy.zeros((enum_total, 3), dtype = numpy.float) # E, dos, idos
+ 
+ for i in range(enum_total):
+     inline_total = inputFile.readline().split()
+     if (subtractFermi == True): 
+       dos[i][0] = float(inline_total[0]) - efermi_total 
+     else: 
+       dos[i][0] = float(inline_total[0])
+     dos[i][1], dos[i][2] = float(inline_total[1]), float(inline_total[2])
+ 
+ # loop through dos, find point where idos > atoms in 3s2 state of S
 
-command_line_counter = commands.getoutput('wc -l ' + input_filename).split()
+ core_valence_divide = 0 # initialize to safe value
 
-if len(command_line_counter) != 2:
-    print 'Error determining file size'
-else:
-    number_of_lines = int(command_line_counter[0])
+ for i in range(enum_total):
+   if ( dos[i][2] > natom*ncore_electrons): 
+     core_valence_divide = i
+     break 
 
-outputFile_dos = open('dos.dat', 'w')
-outputFile_pdos = open('pdos.dat', 'w')
+ efermi_bin = 1e6  # initialize to crazy value
 
-inputFile = open(input_filename, 'r')
+ for i in range(enum_total):
+   if ( dos[i][2] > natom*nelectrons):
+     efermi_bin = i
+     break
 
-natom = int(inputFile.readline().split()[0])
+ if (efermi_bin == 1e6):
+   print 'Fermi bin not found. There is something wrong.'
+   sys.exit(0)
 
-for i in range(4):
-    inputFile.readline()
+ for row in dos:
+     for element in row:
+         outputFile_dos.write(str(element) + ' ')
+     outputFile_dos.write('\n')
+ 
+ ############
+ 
+ enum_project = enum_total
+ 
+ spacing = (emax_total - emin_total)/enum_total
+ pdos = numpy.zeros((enum_project, 1 + 3*2), dtype = numpy.float) # E, s, p, d
+ 
+ for i in range(len(pdos)):
+ 
+    pdos[i][0] = dos[i][0]  # fills in the E column of pdos
+ 
+ for atoms in range(natom):
+     inline_project = inputFile.readline()
+     if len(inline_project) != 70:
+         print 'There are no more atoms in this DOSCAR. Something is wrong.'
+         sys.exit(0)
 
-inline_total = inputFile.readline()
+     for i in range(enum_project):
+         inline_project = inputFile.readline().split()
+         pdos[i][1] += float(inline_project[1]) # s
+         pdos[i][2] += float(inline_project[2]) # p
+         pdos[i][3] += float(inline_project[3]) # d
+ 
+ for i in numpy.arange(1,enum_project):
+     pdos[i][4] = pdos[i - 1][4] + pdos[i][1]
+     pdos[i][5] = pdos[i - 1][5] + pdos[i][2]
+     pdos[i][6] = pdos[i - 1][6] + pdos[i][3]
+ 
+ energies  = numpy.transpose(pdos)[0]
+ s_project = numpy.transpose(pdos)[1]
+ p_project = numpy.transpose(pdos)[2]
+ d_project = numpy.transpose(pdos)[3]
+ 
+ energies_core  =  energies[0:core_valence_divide]
+ s_project_core = s_project[0:core_valence_divide]
+ p_project_core = p_project[0:core_valence_divide]
+ d_project_core = d_project[0:core_valence_divide]
+ 
+ energies_valence  =  energies[core_valence_divide:efermi_bin]
+ s_project_valence = s_project[core_valence_divide:efermi_bin]
+ p_project_valence = p_project[core_valence_divide:efermi_bin]
+ d_project_valence = d_project[core_valence_divide:efermi_bin]
+ 
+ spd_sum_core    = sum(s_project_core)    + sum(p_project_core)    + sum(d_project_core)
+ spd_sum_valence = sum(s_project_valence) + sum(p_project_valence) + sum(d_project_valence)
+ 
+ if (spd_sum_core > 0.):
+   s_fraction_core = sum(s_project_core)/spd_sum_core
+   p_fraction_core = sum(p_project_core)/spd_sum_core
 
-emin_total = float(inline_total.split()[1])
-emax_total = float(inline_total.split()[0])
-enum_total = int(inline_total.split()[2])
-efermi_total = float(inline_total.split()[3])
+ else: 
+   s_fraction_core, p_fraction_core = 0.,0.
+ 
+ if (spd_sum_valence > 0.):
+   s_fraction_valence = sum(s_project_valence)/spd_sum_valence
+   p_fraction_valence = sum(p_project_valence)/spd_sum_valence
+   d_fraction_valence = sum(d_project_valence)/spd_sum_valence
 
-dos = numpy.zeros((enum_total, 3), dtype = numpy.float) # E, dos, idos
-
-for i in range(enum_total):
-    inline_total = inputFile.readline().split()
-#    dos[i][0] = float(inline_total[0])  
-    dos[i][0] = float(inline_total[0]) - efermi_total 
-    dos[i][1], dos[i][2] = float(inline_total[1]), float(inline_total[2])
-
-for row in dos:
-    for element in row:
-        outputFile_dos.write(str(element) + ' ')
-    outputFile_dos.write('\n')
-
-############
-
-
-enum_project = enum_total
-
-spacing = (emax_total - emin_total)/enum_total
-efermi_bin = int((efermi_total - emin_total)/spacing)
-
-pdos = numpy.zeros((enum_project, 1 + 3*2), dtype = numpy.float) # E, s, p, d
-
-for i in range(len(pdos)):
-
-   pdos[i][0] = dos[i][0]  # fills in the E column of pdos
-
-for atoms in range(natom):
-    inline_project = inputFile.readline()
-
-    for i in range(enum_project):
-        inline_project = inputFile.readline().split()
-        pdos[i][1] += float(inline_project[1]) # s
-        pdos[i][2] += float(inline_project[2]) # p
-        pdos[i][3] += float(inline_project[3]) # d
-
-for i in numpy.arange(1,enum_project):
-    pdos[i][4] = pdos[i - 1][4] + pdos[i][1]
-    pdos[i][5] = pdos[i - 1][5] + pdos[i][2]
-    pdos[i][6] = pdos[i - 1][6] + pdos[i][3]
-
-
-energies  = numpy.transpose(pdos)[0]
-s_project = numpy.transpose(pdos)[1]
-p_project = numpy.transpose(pdos)[2]
-d_project = numpy.transpose(pdos)[3]
-
-#energies_core  =  energies[0:core_valence_divide]
-#s_project_core = s_project[0:core_valence_divide]
-#p_project_core = p_project[0:core_valence_divide]
-#d_project_core = d_project[0:core_valence_divide]
-
-
-energies_valence  =  energies[core_valence_divide:efermi_bin]
-s_project_valence = s_project[core_valence_divide:efermi_bin]
-p_project_valence = p_project[core_valence_divide:efermi_bin]
-d_project_valence = d_project[core_valence_divide:efermi_bin]
-
-#spd_sum_core    = sum(s_project_core)    + sum(p_project_core)    + sum(d_project_core)
-spd_sum_valence = sum(s_project_valence) + sum(p_project_valence) + sum(d_project_valence)
-
-#s_fraction_core = sum(s_project_core)/spd_sum_core
-#p_fraction_core = sum(p_project_core)/spd_sum_core
-
-s_fraction_valence = sum(s_project_valence)/spd_sum_valence
-p_fraction_valence = sum(p_project_valence)/spd_sum_valence
-d_fraction_valence = sum(d_project_valence)/spd_sum_valence
-
-#print
-#print "%s core", s_fraction_core*100
-#print "%p core", p_fraction_core*100
-print
-print "%s valence", s_fraction_valence*100
-print "%p valence", p_fraction_valence*100
-print "%d valence", d_fraction_valence*100
-print
-print "norm = ", str(s_fraction_valence + p_fraction_valence + d_fraction_valence)
-print
-print "efermi_total = ", efermi_total
-print "spacing = ", spacing
-print
-
-for row in pdos:
-    for element in row:
-        outputFile_pdos.write(str(element) + ' ')
-    outputFile_pdos.write('\n')
+ else:
+   s_fraction_valence, p_fraction_valence, d_fraction_valence = 0., 0., 0.
+ 
+ print
+ print "%s core", s_fraction_core*100
+ print "%p core", p_fraction_core*100
+ print
+ print "%s valence", s_fraction_valence*100
+ print "%p valence", p_fraction_valence*100
+ print "%d valence", d_fraction_valence*100
+ print
+ print "norm = ", str(s_fraction_valence + p_fraction_valence + d_fraction_valence)
+ print
+ print "efermi_total = ", efermi_total
+ print "spacing = ", spacing
+ print
+ 
+ for row in pdos:
+     for element in row:
+         outputFile_pdos.write(str(element) + ' ')
+     outputFile_pdos.write('\n')
+ 
+ 
+if __name__ == '__main__':
+     main()
